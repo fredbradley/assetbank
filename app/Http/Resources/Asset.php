141db22af512b853238de55682dcee8e10ecf6f3
Fred Bradley CSWEB02 <frb@cranleigh.org>
2020-08-04 10:23:10 +0100

    Asset

--- app/Http/Resources/Asset.php
+++ app/Http/Resources/Asset.php
@@ -4,16 +4,71 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Class Asset
+ * @package App\Http\Resources
+ */
 class Asset extends JsonResource
 {
     /**
+     * @var \Illuminate\Support\Collection
+     */
+    private $attrs;
+
+
+    /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        $this->attrs = collect($this->attributes);
+
+        return [
+            'url' => $this->url,
+            'submitted' => $this->submitted,
+            'approved' => $this->approved,
+            'parents' => $this->parents,
+            'contentUrl' => $this->contentUrl,
+            'displayUrl' => $this->displayUrl,
+            //'attributes' => $this->attributes,
+            'attrs' => $this->getAttrs(),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getAttrs(): array
+    {
+        $attrs = [];
+        foreach ($this->attrs as $attr) {
+            $attrs[ $attr->name ] = $this->getAttribute($attr->name);
+        }
+        return $attrs;
+    }
+
+    /**
+     * @param \App\Http\Resources\string $name
+     *
+     * @return |null
+     */
+    private function getAttribute(string $name)
+    {
+        $filtered = $this->attrs->filter(function ($item) use ($name) {
+            if ($item->name === $name) {
+                return $item;
+            }
+        });
+        $value = $filtered->first()->value;
+        if ($value === "") {
+            return null;
+        }
+        return $value;
+
+
     }
 }
